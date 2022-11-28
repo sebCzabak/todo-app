fa9ca086e49f5c1b224f5a91fa2329904baceeab
sebCzabak <sczabak@gmail.com>
2022-11-28 13:13:30 +0100

    Initial commit

---
+++ src/test/java/sebCzabak/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sebCzabak.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
